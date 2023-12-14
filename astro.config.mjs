b311841141bb1086c5ec9029d733efabe3b80319
waitingwittykitty <hryschiller@gmail.com>
2023-12-14 14:23:40 +0000

    fix: fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 
 export default defineConfig({
-  site: 'https://good-things-astro.com/',
+  site: 'https://good-things-astro.pages.dev/',
   server: {
     host: true,
     port: 3000
--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
   title: `Good Things Foundation`,
   description: `This is a mock business website developed using Astro and Vue 3.`,
-  url: `https://good-things-astro.vercel.app`,
+  url: `https://good-things-astro.pages.dev`,
   name: `GoodThings`,
 };
